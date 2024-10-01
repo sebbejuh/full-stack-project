5b05e5f5fdef2bc686e681a05fee55d35dadf9e8
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-01 18:31:51 +0200

    Created component

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@radix-ui/themes";
+
+const NavBar = () => {
+  return (
+    <nav className="border-b md-5 px-5 py-3">
+      <Container>
+        Navbar
+      </Container>
+    </nav>
+  )
+}
+
+export default NavBar
