999a9a90cf4fca644513aeb241e33c8d93bb52b2
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-01 18:44:35 +0200

    styled post cards

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -1,3 +1,5 @@
+import { Flex, Text, Card, Box, Avatar, Badge } from '@radix-ui/themes';
+import { PersonIcon } from "@radix-ui/react-icons";
 
 interface PostCardProps {
   id: string;
@@ -9,19 +11,34 @@ interface PostCardProps {
   authorImage: string | null;
 }
 
-const PostCard: React.FC<PostCardProps> = ({title, content, createdAt, updatedAt, authorName, authorImage}) => {
+const PostCard: React.FC<PostCardProps> = ({ title, content, createdAt, updatedAt, authorName, authorImage }) => {
   const formattedCreatedAt = new Date(createdAt).toLocaleDateString();
   const formattedUpdatedAt = new Date(updatedAt).toLocaleDateString();
 
   return (
-    <div style={{border: '1px solid white', padding: '15px'}}>
-      <h4>Title: {title}</h4>
-      <p>Content: {content}</p>
-      <p>Created: {formattedCreatedAt}</p>
-      <p>Updated: {formattedUpdatedAt}</p>
-      <p>Author: {authorName}</p>
-      <img style={{width: '50px'}} src={authorImage ?? undefined} alt={authorName ?? 'User Image'} />
-    </div>
+    <Box maxWidth='240px'>
+      <Card>
+        <Flex gap='3' align='center'>
+          <Avatar
+            size='3'
+            src={authorImage ?? undefined}
+            radius='full'
+            fallback={<PersonIcon width="32" height="32" />}
+          />
+          <Text size='1' weight='bold'>{authorName}</Text>
+        </Flex>
+        <Flex gap='3' align='center'>
+          <Text size='3' weight='medium'>{title}</Text>
+        </Flex>
+        <Box>
+          <Text size='1'> {content}</Text>
+          <br/>
+          <Badge>Created: {formattedCreatedAt}</Badge>
+          <br/>
+          <Badge>Updated: {formattedUpdatedAt}</Badge>
+        </Box>
+      </Card>
+    </Box>
   )
 }
 
