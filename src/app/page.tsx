c21dae92c1c719371a572db55c232599af28a890
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-01 18:32:16 +0200

    added radix tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "../../prisma/prisma";
 import PostCard from "./components/PostCard";
-import { Post as PrismaPost} from "@prisma/client";
+import { Post as PrismaPost } from "@prisma/client";
+import { Flex, Text} from '@radix-ui/themes';
 
 type Author = {
   name: string | null;
@@ -27,31 +28,24 @@ export default async function Home() {
   console.log({ posts })
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          <h1>Post feed</h1>
-          {
-            posts.map((post) => {
-              return (
-                <PostCard
-                  key={post.id}
-                  id={post.id}
-                  title={post.title}
-                  content={post.content}
-                  createdAt={post.createdAt}
-                  updatedAt={post.updatedAt}
-                  authorName={post.author?.name ?? null}
-                  authorImage={post.author?.image ?? null}
-                />
-              )
-            })
-          }
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <h1>footer</h1>
-      </footer>
-    </div>
+    <Flex direction='column' align='center' justify='center'>
+      <Text weight='medium' size='5'>Post feed</Text>
+      {
+        posts.map((post) => {
+          return (
+            <PostCard
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              createdAt={post.createdAt}
+              updatedAt={post.updatedAt}
+              authorName={post.author?.name ?? null}
+              authorImage={post.author?.image ?? null}
+            />
+          )
+        })
+      }
+    </Flex>
   );
 }
