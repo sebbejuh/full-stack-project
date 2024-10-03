29533e58e57f2d0e906c4bf997fac79fcf01be45
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-03 19:12:12 +0200

    added signin button

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,10 +1,24 @@
-import { Container } from "@radix-ui/themes";
+'use client'
+import { Container, Button, Flex } from "@radix-ui/themes";
+import { useRouter } from 'next/navigation';
 
 const NavBar = () => {
+  const router = useRouter();
+
+  const handleSignIn = () => {
+    router.push('/api/auth/signin');
+  };
   return (
     <nav className="border-b md-5 px-5 py-3">
       <Container>
-        Navbar
+        <Flex justify='between'>
+          <Flex>
+            Navbar Links
+          </Flex>
+          <Flex>
+            <Button onClick={handleSignIn}>Sign In</Button>
+          </Flex>
+        </Flex>
       </Container>
     </nav>
   )
