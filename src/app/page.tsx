420c74e0df85c503ee0e72e00e6906cd46696b58
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-03 20:10:43 +0200

    Added display of email of logged in user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import prisma from "../../prisma/prisma";
 import PostCard from "./components/PostCard";
 import { Post as PrismaPost } from "@prisma/client";
 import { Flex, Text} from '@radix-ui/themes';
+import { getServerSession } from "next-auth";
+import authOptions from "./auth/authOptions";
 
 type Author = {
   name: string | null;
@@ -25,11 +27,13 @@ async function getPosts(): Promise<PostWithAuthor[]> {
 
 export default async function Home() {
   const posts: PostWithAuthor[] = await getPosts();
-  console.log({ posts })
+  const session = await getServerSession(authOptions)
+
 
   return (
     <Flex direction='column' align='center' justify='center'>
       <Text weight='medium' size='5'>Post feed</Text>
+      <Text>Welcome {session?.user?.email}</Text>
       {
         posts.map((post) => {
           return (
