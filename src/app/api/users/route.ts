f040aba7605dea18e80875f01ad5028bd0b5a94e
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-03 20:16:21 +0200

    fixed minor syntax issues

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,9 @@
 // import prisma from "@/prisma/client";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from '../../../../prisma/prisma'
 
 
-export async function GET(req: NextRequest){
+export async function GET(){
     const users = await prisma.user.findMany({orderBy: {name:"asc"}})
 
     return NextResponse.json(users);
--- src/app/auth/authOptions.ts
+++ src/app/auth/authOptions.ts
@@ -13,7 +13,9 @@ const authOptions: NextAuthOptions = {
     ],
     callbacks: {
         session: async ({ session, token }) => {
-            session.user && (session.user.id = token.sub!)
+          if (session.user) {
+            session.user.id = token.sub!;
+          }
             return session
         },
       },
