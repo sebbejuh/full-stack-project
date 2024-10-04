075d20457d2043e680575483223ebc245315abb5
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-04 18:14:35 +0200

    added logout

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,27 +1,59 @@
 'use client'
-import { Container, Button, Flex } from "@radix-ui/themes";
-import { useRouter } from 'next/navigation';
+import Skeleton from "@/app/components/Skeleton"
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
 
 const NavBar = () => {
-  const router = useRouter();
-
-  const handleSignIn = () => {
-    router.push('/api/auth/signin');
-  };
   return (
     <nav className="border-b md-5 px-5 py-3">
       <Container>
         <Flex justify='between'>
           <Flex>
-            Navbar Links
+            Navbar Links Placeholder
           </Flex>
           <Flex>
-            <Button onClick={handleSignIn}>Sign In</Button>
+            <AuthStatus />
           </Flex>
         </Flex>
       </Container>
     </nav>
   )
 }
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return <Skeleton width="3rem" />;
+  if (status === "unauthenticated" || !session || !session.user)
+    return (
+      <Link className="nav-link" href={"/api/auth/signin"}>
+        Login
+      </Link>
+    );
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={"/api/auth/signout"}>Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
 
 export default NavBar
