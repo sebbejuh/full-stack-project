86e67e9e3a2f1619768c53302337a159813e4d68
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-04 19:17:12 +0200

    added nav-links & styled login button

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -2,15 +2,23 @@
 import Skeleton from "@/app/components/Skeleton"
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
+import { Avatar, Box, Container, DropdownMenu, Flex, Text, Badge } from "@radix-ui/themes";
+import { HomeIcon } from '@radix-ui/react-icons'
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+import { FcGoogle } from "react-icons/fc";
+
 
 const NavBar = () => {
   return (
     <nav className="border-b md-5 px-5 py-3">
       <Container>
         <Flex justify='between'>
-          <Flex>
-            Navbar Links Placeholder
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <HomeIcon width="18" height="18"/>
+            </Link>
+            <NavLinks />
           </Flex>
           <Flex>
             <AuthStatus />
@@ -26,8 +34,8 @@ const AuthStatus = () => {
   if (status === "loading") return <Skeleton width="3rem" />;
   if (status === "unauthenticated" || !session || !session.user)
     return (
-      <Link className="nav-link" href={"/api/auth/signin"}>
-        Login
+      <Link className="nav-link flex flex-row items-center gap-2	" href={"/api/auth/signin"}>
+        <Badge size="3" ><FcGoogle size={22}/>Login</Badge>
       </Link>
     );
   return (
@@ -53,6 +61,31 @@ const AuthStatus = () => {
         </DropdownMenu.Content>
       </DropdownMenu.Root>
     </Box>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Home", href: "/" },
+    { label: "Posts", href: "/posts" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
