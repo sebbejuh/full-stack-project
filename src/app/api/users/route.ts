1cebf1305140cf66bc92bf858d5d0bcbd894c550
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-07 20:06:19 +0200

    commented out unneccessary post api function

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,41 +1,39 @@
-// import prisma from "@/prisma/client";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 import prisma from '../../../../prisma/prisma'
 
 
 export async function GET(){
-    const users = await prisma.user.findMany({orderBy: {name:"asc"}})
+    const users = await prisma.user.findMany({orderBy: {name:'asc'}})
 
     return NextResponse.json(users);
 }
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const { name, email, image } = body
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
+//     const { name, email, image } = body
 
-    //check if email exists
-    const existinUserEmail = await prisma.user.findUnique({
-      where: { email: email }
-    });
-    if (existinUserEmail) {
-      return NextResponse.json({ user: null, message: 'User with this email already exists' }, { status: 409 })
-    }
+//     const existinUserEmail = await prisma.user.findUnique({
+//       where: { email: email }
+//     });
+//     if (existinUserEmail) {
+//       return NextResponse.json({ user: null, message: 'User with this email already exists' }, { status: 409 })
+//     }
 
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        email,
-        image
-      }
-    })
+//     const newUser = await prisma.user.create({
+//       data: {
+//         name,
+//         email,
+//         image
+//       }
+//     })
 
-    return NextResponse.json({ user: newUser, message: 'User has been created' }, { status: 201 });
-  } catch (error) {
-    console.error('Error creating user:', error);
-    return NextResponse.json(
-      { user: null, message: 'An error occurred while creating the user' },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ user: newUser, message: 'User has been created' }, { status: 201 });
+//   } catch (error) {
+//     console.error('Error creating user:', error);
+//     return NextResponse.json(
+//       { user: null, message: 'An error occurred while creating the user' },
+//       { status: 500 }
+//     );
+//   }
+// }
