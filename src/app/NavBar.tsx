4267f135639209a8ef1c33d2ebdbbec2e7ea1bc1
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-14 17:38:10 +0200

    changed size of skeleton

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,22 +1,22 @@
 'use client'
-import Skeleton from "@/app/components/Skeleton"
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import { Avatar, Box, Container, DropdownMenu, Flex, Text, Badge } from "@radix-ui/themes";
+import Skeleton from '@/app/components/Skeleton'
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text, Badge } from '@radix-ui/themes';
 import { HomeIcon } from '@radix-ui/react-icons'
-import { usePathname } from "next/navigation";
-import classNames from "classnames";
-import { FcGoogle } from "react-icons/fc";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
+import { FcGoogle } from 'react-icons/fc';
 
 
 const NavBar = () => {
   return (
-    <nav className="border-b md-5 px-5 py-3">
+    <nav className='border-b md-5 px-5 py-3'>
       <Container>
         <Flex justify='between'>
-          <Flex align="center" gap="3">
-            <Link href="/">
-              <HomeIcon width="18" height="18"/>
+          <Flex align='center' gap='3'>
+            <Link href='/'>
+              <HomeIcon width='18' height='18' />
             </Link>
             <NavLinks />
           </Flex>
@@ -31,11 +31,11 @@ const NavBar = () => {
 
 const AuthStatus = () => {
   const { status, data: session } = useSession();
-  if (status === "loading") return <Skeleton width="3rem" />;
-  if (status === "unauthenticated" || !session || !session.user)
+  if (status === 'loading') return <Skeleton width='2rem' height='2remF' />;
+  if (status === 'unauthenticated' || !session || !session.user)
     return (
-      <Link className="nav-link flex flex-row items-center gap-2	" href={"/api/auth/signin"}>
-        <Badge size="3" ><FcGoogle size={22}/>Login</Badge>
+      <Link className='nav-link flex flex-row items-center gap-2	' href={'/api/auth/signin'}>
+        <Badge size='3' ><FcGoogle size={22} />Login</Badge>
       </Link>
     );
   return (
@@ -44,19 +44,19 @@ const AuthStatus = () => {
         <DropdownMenu.Trigger>
           <Avatar
             src={session!.user!.image!}
-            fallback="?"
-            size="2"
-            radius="full"
-            className="cursor-pointer"
-            referrerPolicy="no-referrer"
+            fallback='?'
+            size='2'
+            radius='full'
+            className='cursor-pointer'
+            referrerPolicy='no-referrer'
           />
         </DropdownMenu.Trigger>
         <DropdownMenu.Content>
           <DropdownMenu.Label>
-            <Text size="2">{session!.user!.email}</Text>
+            <Text size='2'>{session!.user!.email}</Text>
           </DropdownMenu.Label>
           <DropdownMenu.Item>
-            <Link href={"/api/auth/signout"}>Log out</Link>
+            <Link href={'/api/auth/signout'}>Log out</Link>
           </DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
@@ -67,17 +67,17 @@ const AuthStatus = () => {
 const NavLinks = () => {
   const currentPath = usePathname();
   const links = [
-    { label: "Home", href: "/" },
-    { label: "Posts", href: "/posts" },
+    { label: 'Home', href: '/' },
+    { label: 'Posts', href: '/posts' },
   ];
   return (
-    <ul className="flex space-x-6">
+    <ul className='flex space-x-6'>
       {links.map((link) => (
         <li key={link.href}>
           <Link
             className={classNames({
-              "nav-link": true,
-              "!text-zinc-900": link.href === currentPath,
+              'nav-link': true,
+              '!text-zinc-900': link.href === currentPath,
             })}
             href={link.href}
           >
