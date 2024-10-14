ed02ef9c1b6e2952ec6a9ae75e362fcb35b37315
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-14 17:28:50 +0200

    added some formatting of dates on client side.

--- src/app/components/ClientTimezoneDate.tsx
+++ src/app/components/ClientTimezoneDate.tsx
@@ -5,11 +5,34 @@ interface ClientTimezoneProps {
 }
 
 const ClientTimezoneDate: React.FC<ClientTimezoneProps> = ({ date }) => {
-  const convertToUserTimezone = (dateString: string | Date): string => {
+  const formatDateAtUserTimezone = (dateString: string | Date): string => {
     const date = new Date(dateString);
-    return date.toLocaleString();
+    const now = new Date();
+
+    const isToday = date.toDateString() === now.toDateString();
+    const isYesterday = date.toDateString() === new Date(now.setDate(now.getDate() - 1)).toDateString();
+    const isThisYear = date.getFullYear() === now.getFullYear();
+
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const day = date.getDate();
+
+    if (isToday) {
+      return `Today ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
+    }
+
+    if (isYesterday) {
+      return `Yesterday ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
+    }
+
+    if (isThisYear) {
+      return `${monthNames[date.getMonth()]} ${day} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
+    }
+
+    return `${day} ${date.getFullYear()} ${monthNames[date.getMonth()]} ${day} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}`;
   };
-  return <>{convertToUserTimezone(date)}</>;
+
+  return <>{formatDateAtUserTimezone(date)}</>;
 };
 
 export default ClientTimezoneDate;
