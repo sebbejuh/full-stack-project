eecd49602eac98f56f26605b8faa21654e7dc410
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-15 18:03:07 +0200

    Added DeletePostBtn shown if user is posts author

--- src/app/posts/PostCard.tsx
+++ src/app/posts/PostCard.tsx
@@ -2,6 +2,9 @@ import { Flex, Text, Card, Box, Avatar, Badge } from '@radix-ui/themes';
 import { PersonIcon } from "@radix-ui/react-icons";
 import { Post as PrismaPost } from '@prisma/client';
 import ClientTimezoneDate from '../components/ClientTimezoneDate';
+import DeletePostBtn from './DeletePostBtn';
+import { getServerSession } from "next-auth";
+import authOptions from "../auth/authOptions";
 
 type Author = {
   name: string | null;
@@ -14,10 +17,9 @@ interface PostCardProps {
   post: PostWithAuthor;
 }
 
-const PostCard: React.FC<PostCardProps> = ({ post }) => {
-
-  const { title, content, createdAt, updatedAt, author } = post;
-
+const PostCard: React.FC<PostCardProps> = async ({ post }) => {
+  const { id, title, content, createdAt, updatedAt, author, authorId } = post;
+  const session = await getServerSession(authOptions)
   const isSameDate = new Date(createdAt).getTime() === new Date(updatedAt).getTime();
   const mostRecentDate = isSameDate ? createdAt : (new Date(updatedAt).getTime() > new Date(createdAt).getTime() ? updatedAt : createdAt);
   const isUpdated = !isSameDate && new Date(updatedAt).getTime() > new Date(createdAt).getTime();
@@ -26,19 +28,24 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
     <Box width='100%' maxWidth='600px'>
       <Card>
         <Flex direction='column' gap='2'>
-          <Flex gap='3' align='center'>
-            <Avatar
-              size='3'
-              src={author?.image ?? undefined}
-              radius='full'
-              fallback={<PersonIcon width="32" height="32" />}
-            />
-            <Flex direction='column' gap='1'>
-              <Text size='2' weight='bold'>{author?.name}</Text>
-              <Flex>
-                <Badge >{isUpdated && 'Updated at'}<ClientTimezoneDate date={mostRecentDate} /></Badge>
+          <Flex justify='between'>
+            <Flex gap='3' align='center'>
+              <Avatar
+                size='3'
+                src={author?.image ?? undefined}
+                radius='full'
+                fallback={<PersonIcon width="32" height="32" />}
+              />
+              <Flex direction='column' gap='1'>
+                <Text size='2' weight='bold'>{author?.name}</Text>
+                <Flex>
+                  <Badge >{isUpdated && 'Updated at'}<ClientTimezoneDate date={mostRecentDate} /></Badge>
+                </Flex>
               </Flex>
             </Flex>
+            {session && session.user?.id == authorId && (
+              <DeletePostBtn postId={id} />
+            )}
           </Flex>
           <Flex direction='column' gap='1'>
             <Flex align='center'>
