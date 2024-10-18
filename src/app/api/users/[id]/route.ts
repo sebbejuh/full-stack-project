8482c60849b60ff0b3a56edaf889747a588689a9
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-18 16:58:58 +0200

    created api route delete function for user

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from '../../../../../prisma/prisma';
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
+
+export async function DELETE(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user || !session.user.id)
+    return NextResponse.json({ error: 'Not logged in' }, { status: 401 });
+
+  const body = await req.json();
+  const { userId } = body;
+
+  if (!userId)
+    return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    if (user.id !== session.user.id) {
+      return NextResponse.json({ error: 'Wrong user ID' }, { status: 403 });
+    }
+
+    if (user.email) {
+      await prisma.verificationToken.deleteMany({
+        where: { identifier: user.email },
+      });
+    }
+
+    await prisma.user.delete({
+      where: { id: userId },
+      include: {
+        accounts: true,
+        sessions: true,
+        posts: true,
+      },
+    });
+
+    return NextResponse.json({ message: 'User deleted successfully' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Error deleting user', details: error }, { status: 500 });
+  }
+}
