0d5a0abb9a4219412057b36ead830831e0601046
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-25 18:15:20 +0200

    small styling changes to center content on medium devices.

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -9,8 +9,8 @@ const HeroSection = () => {
   return (
     <Flex className='border-b border-neutral-900 pb-4 lg:mb35'>
       <Flex wrap='wrap'>
-        <Flex wrap='wrap' className='w-full lg:w-1/2'>
-          <Flex wrap='wrap' direction='column' className='items-center lg:items-start'>
+        <Flex wrap='wrap' className='w-full lg:w-1/2 items-center lg:items-start justify-center lg:justify-start'>
+          <Flex wrap='wrap' direction='column' className='items-center lg:items-start justify-center lg:justify-start'>
             <Heading as='h1' className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Sebastian Locksenius</Heading>
             <Text className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
               Frontend Developer
@@ -28,10 +28,10 @@ const HeroSection = () => {
             </Text>
           </Flex>
         </Flex>
-        <Flex wrap='wrap' className='w-full lg:w-1/2'>
+        <Flex wrap='wrap' className='w-full lg:w-1/2 items-center lg:items-start justify-center lg:justify-start'>
           <Flex wrap='wrap' justify='center' direction='column' align='center' className='items-center'>
             <Heading as='h2' className='pt-16 pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>My Skills</Heading>
-            <Flex gap='4' wrap='wrap' justify='start' maxWidth='600px'>
+            <Flex gap='4' wrap='wrap' maxWidth='600px' className='justify-center lg:justify-start'>
               <Card><Flex gap='1' align='center'><SiTypescript size='1.35rem' /><Text>TypeScript</Text></Flex></Card>
               <Card><Flex gap='1' align='center'><FaHtml5 size='1.35rem' /><Text>HTML</Text></Flex></Card>
               <Card><Flex gap='1' align='center'><SiCss3 size='1.35rem' /><Text>CSS</Text></Flex></Card>
