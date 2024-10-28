e1f3d0535cea575d638afbc6fda23a8d8a21a9e8
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-28 17:32:51 +0100

    imported radix theme color to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import { blackA, violet, mauve } from "@radix-ui/colors";
+import { blackA, violet, mauve, indigo } from "@radix-ui/colors";
 
 const config: Config = {
   content: [
@@ -13,8 +13,21 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         ...blackA,
-				...violet,
-				...mauve,
+        ...violet,
+        ...mauve,
+        ...indigo,
+        'radix-indigo-1': indigo.indigo1,
+        'radix-indigo-2': indigo.indigo2,
+        'radix-indigo-3': indigo.indigo3,
+        'radix-indigo-4': indigo.indigo4,
+        'radix-indigo-5': indigo.indigo5,
+        'radix-indigo-6': indigo.indigo6,
+        'radix-indigo-7': indigo.indigo7,
+        'radix-indigo-8': indigo.indigo8,
+        'radix-indigo-9': indigo.indigo9,
+        'radix-indigo-10': indigo.indigo10,
+        'radix-indigo-11': indigo.indigo11,
+        'radix-indigo-12': indigo.indigo12,
       },
     },
   },
