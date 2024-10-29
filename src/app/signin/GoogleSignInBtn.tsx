9431e83f47a0eb04e165f867b06cb69c5e35105b
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-29 20:53:30 +0100

    Disabled button when signIn function is running to prevent spam

--- src/app/signin/GoogleSignInBtn.tsx
+++ src/app/signin/GoogleSignInBtn.tsx
@@ -1,15 +1,28 @@
 'use client'
-
+import { useState } from 'react';
 import { Text, Button, Skeleton } from '@radix-ui/themes';
 import { BsGoogle } from 'react-icons/bs';
 import { signIn } from 'next-auth/react'
 import { useSession } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
 
 const GoogleSignInBtn = () => {
   const { status, data: session } = useSession();
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/'
+
+  const handleSignIn = async () => {
+    setIsSigningIn(true);
+    try {
+      await signIn('google', { callbackUrl });
+    } finally {
+      setIsSigningIn(false);
+    }
+  };
   return (
     <Skeleton loading={status === 'loading'}>
-      <Button disabled={!!session} variant='solid' size='4' className=' w-full py-8 justify-center flex flex-row items-center cursor-pointer' onClick={() => signIn('google')}>
+      <Button disabled={!!session || isSigningIn} variant='solid' size='4' className=' w-full py-8 justify-center flex flex-row items-center cursor-pointer' onClick={handleSignIn}>
         <BsGoogle size={27} /> <Text size='5'>Sign in with Google</Text>
       </Button>
     </Skeleton>
