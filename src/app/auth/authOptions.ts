a69e220753d9b8c221e61ef638345f94e016d0d4
Sebastian Locksenius <slocksenius@gmail.com>
2024-10-29 20:52:38 +0100

    added signIn page

--- src/app/auth/authOptions.ts
+++ src/app/auth/authOptions.ts
@@ -4,25 +4,28 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-          })
-    ],
-    callbacks: {
-        session: async ({ session, token }) => {
-          if (session.user) {
-            session.user.id = token.sub!;
-          }
-            return session
-        },
-      },
-    session:{
-        strategy: "jwt"
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.sub!;
+      }
+      return session
     },
-    secret: process.env.NEXTAUTH_SECRET,
+  },
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/signin',
+  },
 }
 
 export default authOptions;
