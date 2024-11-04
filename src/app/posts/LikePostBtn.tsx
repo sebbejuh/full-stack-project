964915ff1fd39547238db9d4be89362d8886d35e
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-04 20:40:12 +0100

    Added tooltip displaying users who liked the post

--- src/app/posts/LikePostBtn.tsx
+++ src/app/posts/LikePostBtn.tsx
@@ -1,17 +1,40 @@
 'use client'
-import { IconButton, Skeleton, Text } from '@radix-ui/themes';
+import { IconButton, Skeleton, Text, Tooltip } from '@radix-ui/themes';
 import { AiOutlineLike } from "react-icons/ai";
 import { useSession } from 'next-auth/react';
+import { Like } from '@prisma/client';
 
-const LikePostBtn = () => {
+type LikeWithUser = Like & {
+  user: {
+    name: string | null;
+  };
+};
+interface LikeBtnProps {
+  likes: LikeWithUser[]
+}
+const generateTooltipContent = (likes: LikeWithUser[]): string => {
+  const likeCount = likes.length;
+  if (likeCount === 0) {
+    return '0 likes';
+  }
+  const displayedLikes = likes.slice(0, 4).map(like => like.user.name).filter(Boolean).join(', ');
+  const additionalLikes = likeCount > 4 ? `, and ${likeCount - 4} others have liked this post` : '';
+  return displayedLikes + additionalLikes;
+}
+
+const LikePostBtn = ({ likes }: LikeBtnProps) => {
   const { status, data: session } = useSession();
-
+  const likeCount = likes.length;
+  const tooltipContent = generateTooltipContent(likes);
   return (
     <Skeleton loading={status === 'loading'}>
-      <IconButton variant="solid" className='hover:cursor-pointer w-full px-2 gap-1' disabled={!session}>
-        <AiOutlineLike />
-        <Text size='1' >Like</Text>
-      </IconButton>
+      <Tooltip content={tooltipContent}>
+        <IconButton variant="solid" className='hover:cursor-pointer w-full px-2 gap-1' disabled={!session}>
+          <AiOutlineLike />
+          <Text size='1' >Like</Text>
+          <Text size='1' >({likeCount})</Text>
+        </IconButton>
+      </Tooltip>
     </Skeleton >
   )
 }
