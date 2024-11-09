0d04c93a433d4695d44e9ccc1bab91c87aeedb9a
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-09 02:25:17 +0100

    Moved advanced tooltip to another component

--- src/app/posts/LikePostBtn.tsx
+++ src/app/posts/LikePostBtn.tsx
@@ -12,29 +12,16 @@ type LikeWithUser = Like & {
 interface LikeBtnProps {
   likes: LikeWithUser[]
 }
-const generateTooltipContent = (likes: LikeWithUser[]): string => {
-  const likeCount = likes.length;
-  if (likeCount === 0) {
-    return '0 likes';
-  }
-  const displayedLikes = likes.slice(0, 4).map(like => like.user.name).filter(Boolean).join(', ');
-  const additionalLikes = likeCount > 4 ? `, and ${likeCount - 4} others have liked this post` : '';
-  return displayedLikes + additionalLikes;
-}
 
 const LikePostBtn = ({ likes }: LikeBtnProps) => {
   const { status, data: session } = useSession();
-  const likeCount = likes.length;
-  const tooltipContent = generateTooltipContent(likes);
+
   return (
     <Skeleton loading={status === 'loading'}>
-      <Tooltip content={tooltipContent}>
+      <Tooltip content='I like this'>
         <IconButton variant="solid" className='hover:cursor-pointer w-full px-2 gap-1' disabled={!session}>
           <AiOutlineLike />
-          <Text size='1' >Like</Text>
-          {likeCount > 0 &&
-            <Text size='1'>{likeCount}</Text>
-          }
+          <Text size='1'>Like</Text>
         </IconButton>
       </Tooltip>
     </Skeleton >
