922e1bef4c27eab5060882a23b7e052535d628c9
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-13 02:25:36 +0100

    added motion to component

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,21 +1,34 @@
-import { Flex, Text, Heading, Card, Grid } from '@radix-ui/themes';
-import { SiTypescript, SiCss3, SiPrisma, SiMongodb, SiPostgresql, SiExpress } from "react-icons/si";
-import { FaHtml5, FaReact, FaNodeJs, FaWordpressSimple, FaGitAlt, FaSass } from "react-icons/fa";
-import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
-import { GrMysql } from "react-icons/gr";
-
+'use client'
+import { Flex, Text, Card, } from '@radix-ui/themes';
+import { SiTypescript, SiCss3, SiPrisma, SiMongodb, SiPostgresql, SiExpress } from 'react-icons/si';
+import { FaHtml5, FaReact, FaNodeJs, FaWordpressSimple, FaGitAlt, FaSass } from 'react-icons/fa';
+import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
+import { GrMysql } from 'react-icons/gr';
+import { motion } from 'framer-motion'
 
 const HeroSection = () => {
   return (
-    <Flex justify='center' className='border-b border-neutral-900 pb-4 w-full'>
+    <Flex justify='center' className='border-b border-neutral-900 pb-4 w-full overflow-hidden'>
       <Flex wrap='wrap'>
         <Flex wrap='wrap' className='w-full lg:w-1/2 items-center lg:items-start justify-center lg:justify-start'>
-          <Flex wrap='wrap' direction='column' className='items-center lg:items-start justify-center lg:justify-start'>
-            <Heading as='h1' className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Sebastian Locksenius</Heading>
-            <Text className='bg-gradient-to-r from-radix-indigo-9 via-radix-indigo-7 to-radix-indigo-3 bg-clip-text text-4xl tracking-tight text-transparent'>
+          <div className='flex flex-col items-center lg:items-start justify-center lg:justify-start flex-wrap'>
+            <motion.h1
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5, delay: 0 }}
+              className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Sebastian Locksenius</motion.h1>
+            <motion.span
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5, delay: 0.1 }}
+              className='bg-gradient-to-r from-radix-indigo-9 via-radix-indigo-7 to-radix-indigo-3 bg-clip-text text-4xl tracking-tight text-transparent'>
               Frontend Developer
-            </Text>
-            <Text className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+            </motion.span>
+            <motion.p
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className='my-2 max-w-xl py-6 font-light tracking-tighter'>
               Developer with skills across the stack.
               I take pride in my work and communicate
               effectively in both my native Swedish and
@@ -26,29 +39,50 @@ const HeroSection = () => {
               methods. <br />
               I am passionate about problemsolving and keeping up to date with new
               technologies and concepts
-            </Text>
-          </Flex>
+            </motion.p>
+          </div>
         </Flex>
         <Flex wrap='wrap' className='w-full lg:w-1/2 items-center lg:items-start justify-center lg:justify-end pb-6'>
           <Flex wrap='wrap' justify='center' direction='column' align='center' className='items-center'>
-            <Heading as='h2' className='pt-6 pb-10 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Skills</Heading>
-            <Grid className='grid grid-cols-2 grid-rows-7 md:grid-cols-3 md:grid-rows-6 lg:grid-cols-3 lg:grid-rows-5 gap-4'>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><SiTypescript size='1.35rem' /><Text>TypeScript</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><FaHtml5 size='1.35rem' /><Text>HTML</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><SiCss3 size='1.35rem' /><Text>CSS</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><FaReact size='1.35rem' /><Text>React</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><RiNextjsFill size='1.35rem' /><Text>Next.js</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><FaNodeJs size='1.35rem' /><Text>Node.js</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><SiPrisma size='1.35rem' /><Text>Prisma</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><RiTailwindCssFill size='1.35rem' /><Text>Tailwind</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><FaSass size='1.35rem' /><Text>Sass</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><GrMysql size='1.35rem' /><Text>MySQL</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><SiMongodb size='1.35rem' /><Text>MongoDB</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><SiPostgresql size='1.35rem' /><Text>PostgreSQL</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><SiExpress size='1.35rem' /><Text>Express</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><FaWordpressSimple size='1.35rem' /><Text>Wordpress</Text></Flex></Card>
-              <Card size='2' className='transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300 ...'><Flex gap='1' align='center' justify='center'><FaGitAlt size='1.35rem' /><Text>Git</Text></Flex></Card>
-            </Grid>
+            <motion.h2
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 0.5, delay: 0 }}
+              className='pt-6 pb-10 text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Skills</motion.h2>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 0.5, delay: 0.15 }}
+            >
+              <div className="grid grid-cols-2 grid-rows-7 md:grid-cols-3 md:grid-rows-6 lg:grid-cols-3 lg:grid-rows-5 gap-4">
+                {[
+                  { icon: <SiTypescript size="1.35rem" />, text: 'TypeScript' },
+                  { icon: <FaHtml5 size="1.35rem" />, text: 'HTML' },
+                  { icon: <SiCss3 size="1.35rem" />, text: 'CSS' },
+                  { icon: <FaReact size="1.35rem" />, text: 'React' },
+                  { icon: <RiNextjsFill size="1.35rem" />, text: 'Next.js' },
+                  { icon: <FaNodeJs size="1.35rem" />, text: 'Node.js' },
+                  { icon: <SiPrisma size="1.35rem" />, text: 'Prisma' },
+                  { icon: <RiTailwindCssFill size="1.35rem" />, text: 'Tailwind' },
+                  { icon: <FaSass size="1.35rem" />, text: 'Sass' },
+                  { icon: <GrMysql size="1.35rem" />, text: 'MySQL' },
+                  { icon: <SiMongodb size="1.35rem" />, text: 'MongoDB' },
+                  { icon: <SiPostgresql size="1.35rem" />, text: 'PostgreSQL' },
+                  { icon: <SiExpress size="1.35rem" />, text: 'Express' },
+                  { icon: <FaWordpressSimple size="1.35rem" />, text: 'Wordpress' },
+                  { icon: <FaGitAlt size="1.35rem" />, text: 'Git' },
+                ].map((item, index) => (
+                  <div key={index} className="transition ease-in-out delay-25 hover:-translate-y-1 hover:scale-110 hover:bg-radix-indigo-10 duration-300">
+                    <Card size="2">
+                      <Flex gap="1" align="center" justify="center">
+                        {item.icon}
+                        <Text>{item.text}</Text>
+                      </Flex>
+                    </Card>
+                  </div>
+                ))}
+              </div>
+            </motion.div>
           </Flex>
         </Flex>
       </Flex>
--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -1,109 +1,127 @@
+'use client'
 import { Flex, Text, Heading, Card, Badge, Button, Inset } from '@radix-ui/themes';
 import PostsAppImage from '../../../public/PostsAppImage.png'
 import WebshopImage from '../../../public/WebshopImage.png'
 import Link from 'next/link';
 import { FaGithub } from "react-icons/fa";
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 const ProjectsSection = () => {
   return (
-    <Flex direction='column' className='border-b border-neutral-900 py-8 items-center w-full'>
-      <Heading as='h2' className='text-4xl lg:text-6xl pb-6 lg:pb-12 lg:pt-3 font-thin tracking-tight'>Deployed Projects</Heading>
-      <Flex justify='center' wrap='wrap' className='gap-y-8 gap-x-20 flex-col lg:flex-row items-center lg:items-start'>
+    <Flex direction='column' className='border-b border-neutral-900 py-8 items-center w-full overflow-hidden'>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 0.5 }}
+        className='text-4xl lg:text-6xl pb-6 lg:pb-12 lg:pt-3 font-thin tracking-tight'>Deployed Projects</motion.h2>
+      <Flex justify='center' wrap='wrap' className='gap-y-8 gap-x-20 flex-col lg:flex-row items-center lg:items-start overflow-hidden'>
 
-        <Card className='flex shrink-0 grow-0 sm:w-4/5 md:w-3/5 lg:w-1/3 max-w-[683px] h-full lg:min-h-[725px] items-center lg:items-start justify-center lg:justify-start flex-wrap flex-auto gap-3 flex-col overflow-hidden'>
-          <Inset clip="padding-box" side="top" pb="current">
-            <Flex>
-              <Link href='/posts'>
-                <Image
-                  src={PostsAppImage}
-                  alt='Posts App Image'
-                  className='transition duration-300 ease-in-out hover:scale-105 rounded-t-lg'
-                  width={683}
-                  height={448}
-                  placeholder='blur'
-                />
-              </Link>
-            </Flex>
-          </Inset>
-          <Flex gap='3' direction='column' justify='center' className='items-center w-full'>
-            <Heading as='h3' className='text-1xl font-thin'>Posts App <span className='text-sm'>(in progress)</span></Heading>
-            <Heading size='3' as='h4' className='font-thin'>Full-stack social media web app</Heading>
-            <Link href='https://github.com/sebbejuh/full-stack-project' passHref legacyBehavior>
-              <a target='_blank'>
-                <Button color='purple' className='cursor-pointer'><FaGithub size='1.35rem' />Github</Button>
-              </a>
-            </Link>
-            <Text size='3' className='max-w-xl font-light'>Deployed on this very website using Vercel!</Text>
-            <Card className='flex flex-col justify-center text-center gap-2 w-full'>
-              <Heading size='5' as='h5' className='font-thin underline'>Features</Heading>
-              <Flex direction='column' align='center' justify='center' wrap='wrap'>
-                <Text size='3' className='font-light'>Login and register using google account.</Text>
-                <Text size='3' className='font-light'>Delete user account together with all saved data.</Text>
-                <Text size='3' className='font-light'>Create posts & Delete own posts.</Text>
-                <Text size='3' className='font-light'>Like posts & Unlike posts.</Text>
-              </Flex>
-              <Text size='2' className='font-light'>
-                Coming: Functionality to comment on posts.
-              </Text>
-            </Card>
-            <Flex wrap='wrap' justify='center' gap='2' className='w-full'>
-              <Badge size='3' variant="surface">TypeScript</Badge>
-              <Badge size='3' variant="surface" color='purple'>Next.js</Badge>
-              <Badge size='3' variant="surface" color='jade'>PostgreSQL</Badge>
-              <Badge size='3' variant="surface" color='crimson'>PrismaORM</Badge>
-              <Badge size='3' variant="surface" color='grass'>Tailwind</Badge>
-              <Badge size='3' variant="surface" color='cyan'>Radix-ui</Badge>
-              <Badge size='3' variant="surface" color='amber'>NextAuth</Badge>
-            </Flex>
-          </Flex>
-        </Card>
-
-        <Card className='flex shrink-0 grow-0 sm:w-4/5 md:w-3/5 lg:w-1/3 max-w-[683px] h-full lg:min-h-[766px] items-center lg:items-start justify-center lg:justify-start flex-wrap flex-auto gap-3 flex-col overflow-hidden'>
-          <Inset clip="padding-box" side="top" pb="current">
-            <Flex>
-              <Link href='https://sebbejuh.github.io/webshop-ts/' passHref legacyBehavior>
-                <a target='_blank'>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className='flex shrink-0 grow-0 sm:w-4/5 md:w-3/5 lg:w-1/3 max-w-[683px] h-full lg:min-h-[767px] max-w-full'>
+          <Card className='flex items-center lg:items-start justify-center lg:justify-start flex-wrap flex-auto gap-3 flex-col overflow-hidden'>
+            <Inset clip="padding-box" side="top" pb="current">
+              <Flex>
+                <Link href='/posts'>
                   <Image
-                    src={WebshopImage}
-                    alt='Webshop App Image'
+                    src={PostsAppImage}
+                    alt='Posts App Image'
                     className='transition duration-300 ease-in-out hover:scale-105 rounded-t-lg'
                     width={683}
                     height={448}
                     placeholder='blur'
                   />
+                </Link>
+              </Flex>
+            </Inset>
+            <Flex gap='3' direction='column' justify='center' className='items-center w-full'>
+              <Heading as='h3' className='text-1xl font-thin'>Posts App <span className='text-sm'>(in progress)</span></Heading>
+              <Heading size='3' as='h4' className='font-thin'>Full-stack social media web app</Heading>
+              <Link href='https://github.com/sebbejuh/full-stack-project' passHref legacyBehavior>
+                <a target='_blank'>
+                  <Button color='purple' className='cursor-pointer'><FaGithub size='1.35rem' />Github</Button>
                 </a>
               </Link>
-            </Flex>
-          </Inset>
-          <Flex gap='3' direction='column' className='items-center w-full'>
-            <Heading as='h3' className='text-1xl font-thin'>Webshop App</Heading>
-            <Heading size='3' as='h4' className='font-thin'> Simple Front-end e-commerce web app</Heading>
-            <Link href='https://github.com/sebbejuh/webshop-ts' passHref legacyBehavior>
-              <a target='_blank'>
-                <Button color='purple' className='cursor-pointer'><FaGithub size='1.35rem' />Github</Button>
-              </a>
-            </Link>
-            <Text size='3' className='max-w-xl font-light'>Deployed using GitHub Pages!</Text>
-            <Card className='flex flex-col justify-center text-center gap-2 w-full'>
-              <Heading size='5' as='h5' className='font-thin underline'>Features</Heading>
-              <Flex direction='column' align='center' justify='center' wrap='wrap'>
-                <Text size='3' className='font-light'>Add items to cart, Remove items from cart.</Text>
-                <Text size='3' className='font-light'>Change amount of items in cart which updates price.</Text>
-                <Text size='3' className='font-light'>Create orders from cart, Delete orders from user.</Text>
+              <Text size='3' className='max-w-xl font-light'>Deployed on this very website using Vercel!</Text>
+              <Card className='flex flex-col justify-center text-center gap-2 w-full'>
+                <Heading size='5' as='h5' className='font-thin underline'>Features</Heading>
+                <Flex direction='column' align='center' justify='center' wrap='wrap'>
+                  <Text size='3' className='font-light'>Login and register using google account.</Text>
+                  <Text size='3' className='font-light'>Delete user account together with all saved data.</Text>
+                  <Text size='3' className='font-light'>Create posts & Delete own posts.</Text>
+                  <Text size='3' className='font-light'>Like posts & Unlike posts.</Text>
+                </Flex>
+                <Text size='2' className='font-light'>
+                  Coming: Functionality to comment on posts.
+                </Text>
+              </Card>
+              <Flex wrap='wrap' justify='center' gap='2' className='w-full'>
+                <Badge size='3' variant="surface">TypeScript</Badge>
+                <Badge size='3' variant="surface" color='purple'>Next.js</Badge>
+                <Badge size='3' variant="surface" color='jade'>PostgreSQL</Badge>
+                <Badge size='3' variant="surface" color='crimson'>PrismaORM</Badge>
+                <Badge size='3' variant="surface" color='grass'>Tailwind</Badge>
+                <Badge size='3' variant="surface" color='cyan'>Radix-ui</Badge>
+                <Badge size='3' variant="surface" color='amber'>NextAuth</Badge>
               </Flex>
-              <Text size='2' className='font-light'>
-                Stores user actions in local storage & accesses products from JSON document.
-              </Text>
-            </Card>
-            <Flex wrap='wrap' justify='center' gap='2' className='w-full'>
-              <Badge size='3' variant="surface">TypeScript</Badge>
-              <Badge size='3' variant="surface" color='iris'>React.js</Badge>
-              <Badge size='3' variant="surface" color='crimson'>Sass</Badge>
-              <Badge size='3' variant="surface" color='tomato'>ReactContext</Badge>
             </Flex>
-          </Flex>
-        </Card>
+          </Card>
+        </motion.div>
+
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className='flex shrink-0 grow-0 sm:w-4/5 md:w-3/5 lg:w-1/3 max-w-[683px] h-full lg:min-h-[767px] max-w-full'>
+          <Card className='flex items-center lg:items-start justify-center lg:justify-start flex-wrap flex-auto gap-3 flex-col overflow-hidden'>
+            <Inset clip="padding-box" side="top" pb="current">
+              <Flex>
+                <Link href='https://sebbejuh.github.io/webshop-ts/' passHref legacyBehavior>
+                  <a target='_blank'>
+                    <Image
+                      src={WebshopImage}
+                      alt='Webshop App Image'
+                      className='transition duration-300 ease-in-out hover:scale-105 rounded-t-lg'
+                      width={683}
+                      height={448}
+                      placeholder='blur'
+                    />
+                  </a>
+                </Link>
+              </Flex>
+            </Inset>
+            <Flex gap='3' direction='column' className='items-center w-full'>
+              <Heading as='h3' className='text-1xl font-thin'>Webshop App</Heading>
+              <Heading size='3' as='h4' className='font-thin'> Simple Front-end e-commerce web app</Heading>
+              <Link href='https://github.com/sebbejuh/webshop-ts' passHref legacyBehavior>
+                <a target='_blank'>
+                  <Button color='purple' className='cursor-pointer'><FaGithub size='1.35rem' />Github</Button>
+                </a>
+              </Link>
+              <Text size='3' className='max-w-xl font-light'>Deployed using GitHub Pages!</Text>
+              <Card className='flex flex-col justify-center text-center gap-2 w-full'>
+                <Heading size='5' as='h5' className='font-thin underline'>Features</Heading>
+                <Flex direction='column' align='center' justify='center' wrap='wrap'>
+                  <Text size='3' className='font-light'>Add items to cart, Remove items from cart.</Text>
+                  <Text size='3' className='font-light'>Change amount of items in cart which updates price.</Text>
+                  <Text size='3' className='font-light'>Create orders from cart, Delete orders from user.</Text>
+                </Flex>
+                <Text size='2' className='font-light'>
+                  Stores user actions in local storage & accesses products from JSON document.
+                </Text>
+              </Card>
+              <Flex wrap='wrap' justify='center' gap='2' className='w-full'>
+                <Badge size='3' variant="surface">TypeScript</Badge>
+                <Badge size='3' variant="surface" color='iris'>React.js</Badge>
+                <Badge size='3' variant="surface" color='crimson'>Sass</Badge>
+                <Badge size='3' variant="surface" color='tomato'>ReactContext</Badge>
+              </Flex>
+            </Flex>
+          </Card>
+        </motion.div>
 
       </Flex>
     </Flex>
