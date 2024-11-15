8a9a326f24690887f8b425d72d1f0a9eea56be71
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-15 18:31:28 +0100

    added sorting by user & number of likes

--- src/app/posts/SortPosts.tsx
+++ src/app/posts/SortPosts.tsx
@@ -2,11 +2,14 @@
 import { Button, Skeleton, DropdownMenu } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react'
 import { useRouter, useSearchParams } from "next/navigation";
+import { MdSort } from "react-icons/md";
+
 
 const SortPosts = () => {
   const { status, } = useSession();
   const router = useRouter();
   const searchParams = useSearchParams();
+  const currentSort = searchParams.get('sortPosts') || 'date_desc';
 
   const handleSort = (sortType: string) => {
     const params = new URLSearchParams(searchParams.toString());
@@ -14,22 +17,39 @@ const SortPosts = () => {
     router.push(`?${params.toString()}`);
   };
 
+  const sortLabels: { [key: string]: string } = {
+    date_desc: 'Latest',
+    date_asc: 'Oldest',
+    like_amount: 'Most Liked',
+    author: 'User',
+  };
+
   return (
     <DropdownMenu.Root modal={false}>
       <Skeleton loading={status === 'loading'}>
         <DropdownMenu.Trigger>
-          <Button size='2' variant="outline" className='cursor-pointer'>
-            Sort By
-            <DropdownMenu.TriggerIcon />
+          <Button size='2' color="gray" variant="outline" highContrast className='cursor-pointer'>
+            {sortLabels[currentSort] || 'Latest'}
+            <MdSort />
           </Button>
         </DropdownMenu.Trigger>
       </Skeleton >
       <DropdownMenu.Content>
+        <DropdownMenu.Label className='cursor-pointer'>
+          Sort by :
+        </DropdownMenu.Label>
+        <DropdownMenu.Separator />
         <DropdownMenu.Item className='cursor-pointer' onSelect={() => handleSort('date_desc')}>
           Latest
         </DropdownMenu.Item>
         <DropdownMenu.Item className='cursor-pointer' onSelect={() => handleSort('date_asc')}>
           Oldest
+        </DropdownMenu.Item>
+        <DropdownMenu.Item className='cursor-pointer' onSelect={() => handleSort('like_amount')}>
+          Most Liked
+        </DropdownMenu.Item>
+        <DropdownMenu.Item className='cursor-pointer' onSelect={() => handleSort('author')}>
+          User
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
