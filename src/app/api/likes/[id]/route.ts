fcb03c12cc35feeee5ee151e0f81ab7b964c90b2
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-18 19:28:16 +0100

    changed error message

--- src/app/api/likes/[id]/route.ts
+++ src/app/api/likes/[id]/route.ts
@@ -7,7 +7,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user || !session.user.id)
-    return NextResponse.json({ error: 'Not logged in' }, { status: 401 });
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
   if (!params.id)
     return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
