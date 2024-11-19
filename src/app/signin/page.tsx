0a49e98f3748c344b0b1f3fa6e8b9906ab2d2af5
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-19 22:52:58 +0100

    commented out text

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -12,7 +12,7 @@ const SignInPage = async () => {
               <Flex direction='column' gap='1' justify='center' align='center'>
                 <Text size='1'>By signing in with Google you allow us to save user details from your Google account, such as: your Name, Email and a link to your profile picture.</Text>
                 <Text size='1'>You can always choose to delete these saved user details together with any data related to your account by going to your account page after signing in.</Text>
-                <Text size='3' color='red'>Currently only approved users can sign in to this site.</Text>
+                {/* <Text size='3' color='red'>Currently only approved users can sign in to this site.</Text> */}
               </Flex>
             </Card>
             <Flex>
