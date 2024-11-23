a90819989082b409cabab4d3e76cf851dcdd2109
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-23 18:26:20 +0100

    added comment validation schema

--- src/app/components/validationSchema.ts
+++ src/app/components/validationSchema.ts
@@ -4,3 +4,6 @@ export const postSchema = z.object({
   title: z.string().min(1, { message: 'Title is required', }).max(60, { message: 'Title must be at most 60 characters', }),
   content: z.string().min(1, { message: 'Content is required', }).max(280, { message: 'Content must be at most 280 characters', })
 });
+export const commentSchema = z.object({
+  content: z.string().min(1, { message: 'Content is required', }).max(280, { message: 'Content must be at most 280 characters', })
+});
