66f0ce728ca1d72bd7d779794ee9f36cdbc57c4f
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-23 18:25:51 +0100

    added comments

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -8,6 +8,9 @@ import authOptions from '../../auth/authOptions';
 import LikeCount from '../LikeCount';
 import PostDropDownBtn from '../PostDropDownBtn';
 import LikePostBtn from '../LikePostBtn';
+import CommentCard from './CommentCard';
+
+export const dynamic = 'force-dynamic';
 
 interface Props {
   params: { id: string };
@@ -29,6 +32,13 @@ const page = async ({ params }: Props) => {
             select: { name: true }
           }
         }
+      },
+      comments: {
+        include: {
+          author: {
+            select: { name: true, image: true }
+          }
+        }
       }
     },
   })
@@ -37,51 +47,60 @@ const page = async ({ params }: Props) => {
   const mostRecentDate = isSameDate ? post.createdAt : (new Date(post.updatedAt).getTime() > new Date(post.createdAt).getTime() ? post.updatedAt : post.createdAt);
   const isUpdated = !isSameDate && new Date(post.updatedAt).getTime() > new Date(post.createdAt).getTime();
 
+  const comments = post.comments
   return (
     <Flex direction='column' align='center' gap='4'>
       <Box width='100%' maxWidth='600px'>
-        <Card className='pb-0 px-0'>
-          <Flex direction='column' gap='2' className='px-3 pb-3'>
-            <Flex justify='between'>
-              <Flex gap='3' align='center'>
-                <Avatar
-                  size='3'
-                  src={post.author?.image ?? undefined}
-                  radius='full'
-                  fallback={<PersonIcon width="32" height="32" />}
-                />
-                <Flex direction='column' gap='1'>
-                  <Text size='2' weight='bold'>{post.author?.name}</Text>
-                  <Flex gap='2' align='center'>
-                    <Badge variant='surface' className='text-slate-300'>{isUpdated && 'Updated at'}<ClientTimezoneDate date={mostRecentDate} /></Badge>
-                    {post.likes.length > 0 && (
-                      <LikeCount likes={post.likes} />
-                    )}
+        <Flex direction='column' align='center' gap='4'>
+          <Card className='pb-0 px-0'>
+            <Flex direction='column' gap='2' className='px-3 pb-3'>
+              <Flex justify='between'>
+                <Flex gap='3' align='center'>
+                  <Avatar
+                    size='3'
+                    src={post.author?.image ?? undefined}
+                    radius='full'
+                    fallback={<PersonIcon width="32" height="32" />}
+                  />
+                  <Flex direction='column' gap='1'>
+                    <Text size='2' weight='bold'>{post.author?.name}</Text>
+                    <Flex gap='2' align='center'>
+                      <Badge variant='surface' className='text-slate-300'>{isUpdated && 'Updated at'}<ClientTimezoneDate date={mostRecentDate} /></Badge>
+                      {post.likes.length > 0 && (
+                        <LikeCount likes={post.likes} />
+                      )}
+                    </Flex>
                   </Flex>
                 </Flex>
+                {session && session.user?.id == post.authorId && (
+                  <PostDropDownBtn postId={post.id} />
+                )}
               </Flex>
-              {session && session.user?.id == post.authorId && (
-                <PostDropDownBtn postId={post.id} />
-              )}
-            </Flex>
-            <Flex direction='column' gap='1'>
-              <Flex align='center'>
-                <Text size='3' weight='medium'>{post.title}</Text>
+              <Flex direction='column' gap='1'>
+                <Flex align='center'>
+                  <Text size='3' weight='medium'>{post.title}</Text>
+                </Flex>
+                <Box>
+                  <Text size='2'> {post.content}</Text>
+                </Box>
               </Flex>
-              <Box>
-                <Text size='2'> {post.content}</Text>
-              </Box>
             </Flex>
-          </Flex>
-          <Flex justify='between' style={{ backgroundColor: "#d8f4f609" }} className='px-3 py-2'>
-            <Flex>
-              <LikePostBtn likes={post.likes} postId={post.id} />
-            </Flex>
-            <Flex>
+            <Flex justify='between' style={{ backgroundColor: "#d8f4f609" }} className='px-3 py-2'>
+              <Flex>
+                <LikePostBtn likes={post.likes} postId={post.id} />
+              </Flex>
+              <Flex>
 
-            </Flex>
-          </Flex >
-        </Card>
+              </Flex>
+            </Flex >
+          </Card>
+          <Text>Comments</Text>
+          <Flex direction='column' align='center' gap='2' width='100%'>
+            {comments.map((comment) => (
+              <CommentCard key={comment.id} comment={comment} />
+            ))}
+          </Flex>
+        </Flex>
       </Box >
     </Flex>
   )
