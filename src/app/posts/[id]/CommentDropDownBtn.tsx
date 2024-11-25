f868e1972863050382fe2e3195a33f4a6950662f
Sebastian Locksenius <slocksenius@gmail.com>
2024-11-25 21:44:08 +0100

    Created drop down button for deleting comments

---
+++ src/app/posts/[id]/CommentDropDownBtn.tsx
@@ -0,0 +1,93 @@
+'use client'
+import { AlertDialog, Flex, Button, Skeleton, DropdownMenu, Text } from '@radix-ui/themes';
+import axios, { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useSession } from 'next-auth/react';
+
+const CommentDropDownBtn = ({ commentId }: { commentId: string }) => {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const { status, } = useSession();
+
+  const deleteComment = async () => {
+    try {
+      setLoading(true);
+      await axios.delete("/api/comments/" + commentId, {
+      });
+      toast.success('Comment Deleted!')
+      router.refresh();
+      setLoading(false);
+    } catch (error) {
+      console.error('Error:', error);
+      const axiosError = error as AxiosError<ErrorResponse>;
+      const errorMessage = axiosError.response?.data?.error || 'An unknown error occurred';
+      const errorDetails = axiosError.response?.data?.details?.join(', ') || '';
+      toast.error(`Error: ${errorMessage}${errorDetails ? ` - ${errorDetails}` : ''}`);
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <>
+      <AlertDialog.Root>
+        <DropdownMenu.Root modal={false}>
+          <Skeleton loading={status === 'loading'}>
+            <DropdownMenu.Trigger>
+              <Button size='1' variant="soft" className='cursor-pointer'>
+                <DropdownMenu.TriggerIcon />
+              </Button>
+            </DropdownMenu.Trigger>
+          </Skeleton >
+          <DropdownMenu.Content>
+            <DropdownMenu.Item color="red">
+              <AlertDialog.Trigger>
+                <Text className='cursor-pointer'>Delete</Text>
+              </AlertDialog.Trigger>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this comment? This action cannot be
+            undone.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray" className='hover:cursor-pointer'>
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="tomato" disabled={isLoading} onClick={deleteComment} className='hover:cursor-pointer'>
+                Delete Comment
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This post cannot be deleted.
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            mt="2"
+            onClick={() => setError(false)}
+          >
+            Ok
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  )
+}
+
+export default CommentDropDownBtn
