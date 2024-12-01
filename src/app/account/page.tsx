14792fafa4a51b50bf8b7aa3bf3d778c512c2d64
Sebastian Locksenius <slocksenius@gmail.com>
2024-12-01 19:33:31 +0100

    styled component further

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -24,46 +24,48 @@ export default async function Account() {
   return (
     <Flex direction='column' align='center' justify='center' width='100%'>
       <Box width='100%' height='100%' maxWidth='600px'>
-        <Card>
-          <Flex direction='column' justify='between' width='100%' height='100%' gap='8'>
-            <Flex width='100%' align='center' justify='center'>
-              <Heading as='h1' size='6'>Account Page</Heading>
-            </Flex>
-            <Card>
-              <Flex direction='column' justify='center' width='100%' gap='2'>
-                <Heading as='h2' size='4'>User Details:</Heading>
-                <Flex width='100%' gap='3' align='center'>
-                  <Text>Name:</Text>
-                  <Text size='2'>{session.user?.name}</Text>
-                </Flex>
-                <Flex width='100%' gap='3' align='center'>
-                  <Text>Email:</Text>
-                  <Text size='2'>{session.user?.email}</Text>
-                </Flex>
-                <Flex width='100%' gap='3' align='center'>
-                  <Text>Avatar:</Text>
-                  <Avatar
-                    src={session!.user!.image!}
-                    fallback='?'
-                    size='2'
-                    radius='full'
-                    className='cursor-pointer'
-                    referrerPolicy='no-referrer'
-                  />
-                </Flex>
-                <Flex width='100%' gap='3' align='center'>
-                  <Text>Posts:</Text>
-                  <Text size='2'>{postCount}</Text>
-                </Flex>
-                <Flex width='100%' gap='3' align='center'>
-                  <Text>Likes:</Text>
-                  <Text size='2'>{likeCount}</Text>
-                </Flex>
-              </Flex>
-            </Card>
-            <DeleteUserBtn />
+        <Flex direction='column' justify='between' width='100%' height='100%' gap='6'>
+          <Flex width='100%' align='center' justify='center'>
+            <Heading as='h1' size='6'>Account Page</Heading>
           </Flex>
-        </Card>
+          <Card className='flex justify-center pb-4'>
+            <Flex direction='column' justify='center' gap='2' className='justify-items-center'>
+              <Flex width='100%' gap='3' align='center' justify='center'>
+                <Heading as='h2' size='4'>User Details:</Heading>
+              </Flex>
+              <Flex width='100%' gap='3' align='center'>
+                <Text>Name:</Text>
+                <Text size='2'>{session.user?.name}</Text>
+              </Flex>
+              <Flex width='100%' gap='3' align='center'>
+                <Text>Email:</Text>
+                <Text size='2'>{session.user?.email}</Text>
+              </Flex>
+              <Flex width='100%' gap='3' align='center'>
+                <Text>Avatar:</Text>
+                <Avatar
+                  src={session!.user!.image!}
+                  fallback='?'
+                  size='2'
+                  radius='full'
+                  className='cursor-pointer'
+                  referrerPolicy='no-referrer'
+                />
+              </Flex>
+              <Flex width='100%' gap='3' align='center'>
+                <Text>Posts:</Text>
+                <Text size='2'>{postCount}</Text>
+              </Flex>
+              <Flex width='100%' gap='3' align='center'>
+                <Text>Likes:</Text>
+                <Text size='2'>{likeCount}</Text>
+              </Flex>
+              <Flex align='center' width='100%' gap='2' justify='center'>
+                <DeleteUserBtn />
+              </Flex>
+            </Flex>
+          </Card>
+        </Flex>
       </Box>
     </Flex>
   )
